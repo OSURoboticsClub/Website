17091f39d4cc10963b6b55e955eb5ccfe5929105
John Zeller <johnlzeller@gmail.com>
2014-09-04 05:43:17 -0700

    Initial project page

--- page-templates/project.php
+++ page-templates/project.php
@@ -1,29 +1,34 @@
-<?php
-/**
- * Template Name: Project Page
- */
+<?php get_header(); 
+/*
+Template Name: Project Page
+*/?>
+  <div class="row">
+    <div style="background-image: none;" class="contMain">
+      <div class="main">
+        <div class="left" style="margin:0px 0 0 0;">
+          <div class="section">
+            <h1 style="width: 150%; margin-left: -50%; padding-left: 50%;"><?php the_title() ?></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-5 col-md-push-1"><img class="img-thumbnail" src="<?php echo get_stylesheet_directory_uri() ?>/images/mars.png"></div>
+    <div class="col-md-5 col-md-push-1">
+      <p>
+	      <?php
+	        // Start the Loop.
+	        while ( have_posts() ) : the_post();
 
-get_header(); ?>
+	          // Include the page content template.
+	          the_content();
 
-<div id="main-content" class="main-content">
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					// Include the page content template.
-					the_content();
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
-</div><!-- #main-content -->
-
-<?php
-get_footer();
+	          // If comments are open or we have at least one comment, load up the comment template.
+	          if ( comments_open() || get_comments_number() ) {
+	            comments_template();
+	          }
+	        endwhile;
+	      ?>
+	  	</p>
+    </div>
+  </div>
+<?php get_footer(); ?>
