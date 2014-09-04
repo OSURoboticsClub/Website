7889cd84d2f64eae23e063fe2dd8abdd523ba1f9
John Zeller <johnlzeller@gmail.com>
2014-09-04 02:48:40 -0700

    Small image hack adjust

--- page-templates/about.php
+++ page-templates/about.php
@@ -34,7 +34,7 @@ get_header(); ?>
 					President
 				</td>
 				<td>
-					<img src="<?php echo get_stylesheet_directory_uri() ?>/images/nick.jpg" class="border" style="" alt="Nick">
+					<img src="<?php echo get_stylesheet_directory_uri() ?>/images/nick.jpg" class="border" style="padding-top: 11px;" alt="Nick">
 					<br>
 					<a href="mailto:mccombn@onid.oregonstate.edu">Nick McComb</a><br>
 					Vice President <br>
