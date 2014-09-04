671562db7f6ec7a17e384f987edba5bc5170aa5b
John Zeller <johnlzeller@gmail.com>
2014-09-04 04:58:43 -0700

    Attempting to add footer

--- footer.php
+++ footer.php
@@ -1,5 +1,40 @@
     </div><!-- /.container -->
-
+	<div class="contFooter">
+        <div class="footer">
+            <div class="sectThree">
+                <h3>Navigation</h3>
+                <ul id="internal">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="projects.php">Projects</a></li>
+                    <li><a href="competitions.php">Competitions</a></li>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+                <ul id="external">
+                    <li><a href="http://osumarsrover.com/">Mars Rover website</a></li>
+                    <li><a href="http://oregonstate.edu/groups/osurc/aerial">Autonomous Aerial</a></li>
+                    <li><a href="http://groups.engr.oregonstate.edu/osurc/underwater/">Underwater ROV</a></li>
+                    <!-- <li><a href="http://beaversource.oregonstate.edu/projects/telepresence">Telepresence website</a></li> -->
+                    <!-- <li><a href="#"><br /></a></li> -->
+                </ul>
+            </div>
+        
+            <div class="sectThree">
+                <h3>Join Our Mailing List</h3>
+                <ul>
+                    <li>If you would like to join our mailing list, go <a href="https://secure.engr.oregonstate.edu/mailman/listinfo/osurcmembers">here</a>.</li>
+                </ul>
+            </div>
+            
+            <div class="sectThree">
+                <h3>Meeting Dates</h3>
+                <ul>
+                    Meeting times to be announced.
+                </ul>
+            </div>
+        </div>
+    </div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
