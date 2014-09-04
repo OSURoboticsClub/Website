ae33c1b754acacbc58ef496caa42c7ea739b6bef
John Zeller <johnlzeller@gmail.com>
2014-09-04 00:48:31 -0700

    fixed the content showing

--- page-templates/about.php
+++ page-templates/about.php
@@ -13,7 +13,7 @@ get_header(); ?>
 				while ( have_posts() ) : the_post();
 
 					// Include the page content template.
-					get_template_part( 'content', 'page' );
+					the_content();
 
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) {
