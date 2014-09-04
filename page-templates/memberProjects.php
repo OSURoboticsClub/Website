959d03588d8fe09e6df7f8f7b30e4b04f855c8b1
John Zeller <johnlzeller@gmail.com>
2014-09-04 01:04:26 -0700

    Initial commit of page templates for all pages

---
+++ page-templates/memberProjects.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Member Projects Page
+ */
+
+get_header(); ?>
+
+<div id="main-content" class="main-content">
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					// Include the page content template.
+					the_content();
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				endwhile;
+			?>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+</div><!-- #main-content -->
+
+<?php
+get_sidebar();
+get_footer();
