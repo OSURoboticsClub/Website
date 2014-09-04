1eec8426cbab72c05bcc84f55db558c639b30b6a
John Zeller <johnlzeller@gmail.com>
2014-09-04 01:50:56 -0700

    Adding custom format to about page

--- page-templates/about.php
+++ page-templates/about.php
@@ -6,24 +6,25 @@
 get_header(); ?>
 
   <div class="row">
-    <div class="col-md-5 col-md-push-1"><img class="img-thumbnail" src="<?php echo get_stylesheet_directory_uri() ?>/images/keypeople.png"></div>
     <div class="col-md-5 col-md-push-1">
-      <p>
-		<?php
-			// Start the Loop.
-			while ( have_posts() ) : the_post();
+    	<img class="img-thumbnail" src="<?php echo get_stylesheet_directory_uri() ?>/images/keypeople.png">
+		<p>
+			<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
 
-				// Include the page content template.
-				the_content();
+					// Include the page content template.
+					the_content();
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-			endwhile;
-		?>
-      </p>
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				endwhile;
+			?>
+		</p>
     </div>
+    <div class="col-md-5 col-md-push-1"><?php get_sidebar( 'about-page-officers' ); ?></div>
   </div>
 
 <?php
