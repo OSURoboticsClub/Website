4ecd943bfed5ef8ceb772d663c4ff083f2b93aa3
John Zeller <johnlzeller@gmail.com>
2014-09-04 03:01:08 -0700

    Adding the home page intro widget

--- functions.php
+++ functions.php
@@ -6,6 +6,14 @@
 function wpb_widgets_init() {
 
 	register_sidebar( array(
+		'name' => __( 'Home Page Intro Widget', 'wpb' ),
+		'id' => 'home-page-intro',
+		'description' => __( 'This is the Home page intro widget', 'wpb' ),
+		'before_widget' => '<div>',
+		'after_widget' => '</div>',
+	) );
+
+	register_sidebar( array(
 		'name' => __( 'About Page Officers Widget', 'wpb' ),
 		'id' => 'about-page-officers',
 		'description' => __( 'This is a the About page officers widget', 'wpb' ),
