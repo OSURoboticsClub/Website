79542aa9439e7f122ccbcdfab3fd60acc6122749
John Zeller <johnlzeller@gmail.com>
2014-09-04 17:45:47 -0700

    Fixed up Contacts, Project pages and Gallery

--- page-templates/gallery.php
+++ page-templates/gallery.php
@@ -1,29 +1,45 @@
-<?php
-/**
- * Template Name: Gallery Page
- */
+<?php get_header(); 
+/*
+Template Name: Gallery Page
+*/?>
+  <div class="row">
+    <div style="background-image: none;" class="contMain">
+      <div class="main">
+        <div class="left" style="margin:0px 0 0 0;">
+          <div class="section">
+            <h1 style="width: 150%; margin-left: -50%; padding-left: 50%; margin-top: -30px;"><?php the_title() ?></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-10 col-md-push-1">
+	      <?php
+	        // Start the Loop.
+	        while ( have_posts() ) : the_post();
 
-get_header(); ?>
+	          // Include the page content template.
+	          the_content();
 
-<div id="main-content" class="main-content">
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
+	          // If comments are open or we have at least one comment, load up the comment template.
+	          if ( comments_open() || get_comments_number() ) {
+	            comments_template();
+	          }
+	        endwhile;
+$media_query = new WP_Query(
+    array(
+        'post_type' => 'attachment',
+        'post_status' => 'inherit',
+        'posts_per_page' => -1,
+    )
+);
+$list = array();
+foreach ($media_query->posts as $post) {
+    $list[] = [wp_get_attachment_image_src( $post->ID, 'thumbnail' )[0], wp_get_attachment_url( $post->ID )];
+}
+foreach ($list as $imageurl) { ?>
+    <a href='<?php echo $imageurl[1]; ?>'><img src='<?php echo $imageurl[0]; ?>' style="width: 150px; height: 150px;"></a>
+<?php } ?>
+    </div>
+  </div>
+<?php get_footer(); ?>
 
-					// Include the page content template.
-					the_content();
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
-</div><!-- #main-content -->
-
-<?php
-get_footer();
--- page-templates/project.php
+++ page-templates/project.php
@@ -3,16 +3,16 @@
 Template Name: Project Page
 */?>
   <div class="row">
-    <div style="background-image: none;" class="contMain">
+    <div style="background-image: none; margin-bottom: -55px;" class="contMain">
       <div class="main">
         <div class="left" style="margin:0px 0 0 0;">
           <div class="section">
-            <h1 style="width: 150%; margin-left: -50%; padding-left: 50%;"><?php the_title() ?></h1>
+            <h1 style="width: 150%; margin-left: -50%; padding-left: 50%; margin-top: -30px;"><?php the_title() ?></h1>
           </div>
         </div>
       </div>
     </div>
-    <div class="col-md-5 col-md-push-1"><img class="img-thumbnail" src="<?php echo get_stylesheet_directory_uri() ?>/images/marsrover.jpg"></div>
+    <div class="col-md-5 col-md-push-1"><img class="img-thumbnail" src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>"></div>
     <div class="col-md-5 col-md-push-1">
       <p>
 	      <?php
@@ -29,6 +29,33 @@ Template Name: Project Page
 	        endwhile;
 	      ?>
 	  	</p>
+    </div>
+  </div>
+  <div class="row">
+    <div style="background-image: none; margin-bottom: -55px;" class="contMain">
+      <div class="main">
+        <div class="left" style="margin:0px 0 0 0;">
+          <div class="section">
+            <h1 style="width: 150%; margin-left: -50%; padding-left: 50%; margin-top: 14px;">Recent Posts</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-10 col-md-push-1">
+      <?php query_posts( array ( 'category_name' => $pagename, 'posts_per_page' => 3 ) ); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+         <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+         <div style="padding-left: 20px;">
+           <small>Posted on <?php the_time('F jS, Y @ g:i a'); ?> by <?php the_author(); ?></small><br><br>
+           <?php the_content(); ?>
+         </div><hr>
+      <?php endwhile; ?>
+      <?php else: ?>
+        <h2>Nothing posted yet! Must be busy building more robot :)</h2>
+      <?php endif; ?>
+      <div class="navigation">
+        <span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
+      </div><!-- /.navigation -->
     </div>
   </div>
 <?php get_footer(); ?>
