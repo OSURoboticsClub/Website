a075d78e88a96499605774148ace25b702ff1e48
John Zeller <johnlzeller@gmail.com>
2014-09-04 17:55:44 -0700

    Modifying job postings page

--- page-templates/jobs.php
+++ page-templates/jobs.php
@@ -5,25 +5,34 @@
 
 get_header(); ?>
 
-<div id="main-content" class="main-content">
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
+  <div class="row">
+    <div style="background-image: none; margin-bottom: -55px;" class="contMain">
+      <div class="main">
+        <div class="left" style="margin:0px 0 0 0;">
+          <div class="section">
+            <h1 style="width: 150%; margin-left: -50%; padding-left: 50%; margin-top: -30px;"><?php the_title() ?></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-5 col-md-push-1">
+      <p>
+              <?php
+                // Start the Loop.
+                while ( have_posts() ) : the_post();
 
-					// Include the page content template.
-					the_content();
+                  // Include the page content template.
+                  the_content();
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
-</div><!-- #main-content -->
+                  // If comments are open or we have at least one comment, load up the comment template.
+                  if ( comments_open() || get_comments_number() ) {
+                    comments_template();
+                  }
+                endwhile;
+              ?>
+                </p>
+    </div>
+    <div class="col-md-5 col-md-push-1"><?php echo do_shortcode( '[contact-form-7 id="189" title="Job Postings"]' ); ?></div>
+  </div>
 
-<?php
-get_footer();
+<?php get_footer(); ?>
