59bb3a256f36419e6b42c96c977cefb4d2509763
Gzxgzxgzx <h@wfvl.de>
2016-02-17 18:06:45 -0800

    Forgot a few things

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
       <!-- PAGE TITLE -->
       <div class="row">
           <div class="header col-md-12">
-            <p><span>Welcome</span> to the OSU Robotics Club website!</h1>
+            <p><span>Welcome</span> to the OSU Robotics Club website!</p>
           </div>
       </div>
       <!-- END PAGE TITLE -->
@@ -98,46 +98,6 @@
         </div>
         <!-- END PROJECTS LINKS -->
       </section>
-        <!--
-        <div class="aerial project col-md-4">
-          <p class="image">
-            <a href="<? echo esc_url( home_url( '/' ) ); ?>aerial"></a>
-          </p>
-          <p class="text">
-            <span class="lquote">&ldquo;</span>
-            The OSU Robotics Club Aerial Team is focused on building custom, autonomous UAV’s.
-            Currently the club has designed indoor and outdoor UAV’s with high payload capacities,
-            full autonomy, and long battery life. The team works towards furthering the understanding
-            of aerial vehicle technologies <span class="ellipsis">[ . . . ]</span> <span class="rquote">&rdquo;</span>
-          </p>
-          <p><a href="<? echo esc_url( home_url( '/' ) ); ?>aerial">Visit here for more.</a></p>
-        </div>
-        <div class="rover project col-md-4">
-          <p class="image">
-            <a href="<? echo esc_url( home_url( '/' ) ); ?>aerial"></a>
-          </p>
-          <p class="text">
-            <span class="lquote">&ldquo;</span>
-            The OSU Mars Rover Team is a distinguished robotics team within the OSURC that creates
-            Mars Rover-like robots to compete in international robotics competitions, namely NASA’s
-            Sample Return Robot Challenge and the Mars Society’s University Rover Challenge
-             <span class="ellipsis">[ . . . ]</span> <span class="rquote">&rdquo;</span>
-          </p>
-          <p><a href="<? echo esc_url( home_url( '/' ) ); ?>rover">Visit here for more.</a></p>
-        </div>
-        <div class="underwater project col-md-4">
-          <p class="image">
-            <a href="<? echo esc_url( home_url( '/' ) ); ?>underwater"></a>
-          </p>
-          <p class="text">
-            <span class="lquote">&ldquo;</span>
-            The OSURC Underwater Team is engineering for the 2015-16 MATE competition. The team will
-             build a small, tethered ROV that can be used to solve today’s problems in the Ocean:
-             re-fitting undersea cable, repair broken oil wellheads, retrieving Cube Satellites, and
-              surveying <span class="ellipsis">[ . . . ]</span> <span class="rquote">&rdquo;</span>
-          </p>
-          <p><a href="<? echo esc_url( home_url( '/' ) ); ?>underwater">Visit here for more.</a></p>
-        </div>-->
       <!-- END PROJECTS -->
     <!-- END INDEX -->
   </div>
