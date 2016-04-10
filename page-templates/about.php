5b9e1fb64b96377415c75b40d044ca225ec8778c
Harlan James <h@wfvl.de>
2016-04-09 23:37:01 -0700

    Continued 'about' testing (unsuccessful), style.scss broken into modules, added officer images

--- page-templates/about.php
+++ page-templates/about.php
@@ -3,6 +3,8 @@
  * Template Name: About
  */
 
+$img = get_stylesheet_directory_uri() . "/images/officers/";
+
 get_header(); ?>
       <div id="about" class="container">
         <div class="row">
@@ -17,28 +19,55 @@ get_header(); ?>
                 the_content();
               endwhile; ?>
           </div>
+        <!--</div>
+        <div style="padding-top: 10px" class="row">-->
           <div class="col-md-6">
             <ul>
               <li>
-                <p><img src="http://osurobotics.club/wp-content/uploads/2015/12/Nick-150x150.jpg"></p>
-                <p><a href="mailto:mccombn@onid.oregonstate.edu">Nick McComb</a></p>
+                <p><img src="<? echo $img . "nick.jpg" ?>"></p>
+                <p><a href="mailto:mccombn@oregonstate.edu">Nick McComb</a></p>
                 <p>President</p>
               </li>
               <li>
-                <p><img src="http://osurobotics.club/wp-content/uploads/2015/12/Dylan-150x150.jpg"></p>
+                <p><img src="<? echo $img . "dylan.jpg" ?>"></p>
                 <p><a href="mailto:dylanthrush1@gmail.com">Dylan Thrush</a></p>
                 <p>Vice-President</p>
               </li>
               <li>
-                <img src="http://osurobotics.club/wp-content/uploads/2015/12/Corwin-150x150.jpg"></p>
+                <img src="<? echo $img . "corwin.jpg" ?>"></p>
                 <p><a href="mailto:caperren@gmail.com">Corwin Perren</a></p>
                 <p>Treasurer</p>
               </li>
               <li>
-                <p><img src="http://osurobotics.club/wp-content/uploads/2015/12/Billy-150x150.jpg"></p>
-                <p><a href="mailto:rycoske@gmail.com">Billy Edwards</a></p>
-                <p>Secretary</p>
+                <p><img src="<? echo $img . "billy.jpg" ?>"></p>
+                <p><a href="mailto:edwardwi@oregonstate.edu">Billy Edwards</a></p>
+                <p>Rover Team Lead</p>
+              </li><!--
+              <li>
+                <p><img src="<? echo $img . "evan.png" ?>"></p>
+                <p><a href="mailto:gonnerme@oregonstate.edu">Evan Gonnerman</a></p>
+                <p>Aerial Team Lead</p>
               </li>
+              <li>
+                <p><img src="<? echo $img . "daniel.png" ?>"></p>
+                <p><a href="mailto:knottd@oregonstate.edu">Daniel Knott</a></p>
+                <p>Underwater Team Lead</p>
+              </li>
+              <li>
+                <p><img src="<? echo $img . "griffin.png" ?>"></p>
+                <p><a href="mailto:cervag@oregonstate.edu">Griffin Cerva</a></p>
+                <p>3D Printing</p>
+              </li>
+              <li>
+                <p><img src="<? echo $img . "ryan.png" ?>"></p>
+                <p><a href="mailto:domresr@oregonstate.edu">Ryan Domres</a></p>
+                <p>Hardware Developer</p>
+              </li>
+              <li>
+                <p><img src="<? echo $img . "jason.png" ?>"></p>
+                <p><a href="#">Jason Lemieux</a></p>
+                <p>Club Accountant</p>
+              </li>-->
             </ul>
           </div>
         </div>
